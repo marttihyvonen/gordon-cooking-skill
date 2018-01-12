83b0535ebcc42b0a0188bfc9e81cb3bc1f755399
Martti Hyvönen <martti.hyvonen@frantic.com>
2018-01-12 19:51:20 +0200

    First version.

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -19,34 +19,24 @@ const Alexa = require('alexa-sdk');
 //Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
 const APP_ID = undefined;
 
-const SKILL_NAME = 'Space Facts';
-const GET_FACT_MESSAGE = "Here's your fact: ";
+const SKILL_NAME = 'Gordon';
+const GET_FACT_MESSAGE = "Here's one great Mac and Cheese recipe. ";
 const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
 const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
 
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/lambda/data
-//=========================================================================================================================================
-const data = [
-    'A year on Mercury is just 88 days long.',
-    'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-    'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-    'On Mars, the Sun appears about half the size as it does on Earth.',
-    'Earth is the only planet not named after a god.',
-    'Jupiter has the shortest day of all the planets.',
-    'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-    'The Sun contains 99.86% of the mass in the Solar System.',
-    'The Sun is an almost perfect sphere.',
-    'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-    'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-    'The temperature inside the Sun can reach 15 million degrees Celsius.',
-    'The Moon is moving approximately 3.8 cm away from our planet every year.',
-];
+var currentStep = 0;
 
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.
-//=========================================================================================================================================
+const macAndCheese = [
+    'Cook macaroni according to the package directions. Drain.',
+    'In a saucepan, melt butter or margarine over medium heat.',
+    'Stir in enough flour to make a roux. Add milk to roux slowly, stirring constantly. ',
+    'Stir in cheeses, and cook over low heat until cheese is melted and the sauce is a little thick. ',
+    'Put macaroni in large casserole dish, and pour sauce over macaroni. Stir well.',
+    'Melt butter or margarine in a skillet over medium heat. Add breadcrumbs and brown.',
+    'Spread over the macaroni and cheese to cover. Sprinkle with a little paprika.',
+    'Bake at 350 degrees F 175 degrees C for 30 minutes. Serve.'
+];
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -57,15 +47,33 @@ exports.handler = function(event, context, callback) {
 
 const handlers = {
     'LaunchRequest': function () {
-        this.emit('GetNewFactIntent');
+        currentStep = 0;
+        this.emit('CurrentStep');
     },
-    'GetNewFactIntent': function () {
-        const factArr = data;
-        const factIndex = Math.floor(Math.random() * factArr.length);
-        const randomFact = factArr[factIndex];
-        const speechOutput = GET_FACT_MESSAGE + randomFact;
+    'CurrentStep': function () {
+        const factArr = macAndCheese;
+        const step = factArr[currentStep];
+        const speechOutput = GET_FACT_MESSAGE + step;
 
-        this.response.cardRenderer(SKILL_NAME, randomFact);
+        this.response.cardRenderer(SKILL_NAME, step);
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+    'NextStep': function () {
+        const factArr = macAndCheese;
+        currentStep = currentStep + 1;
+        const step = factArr[currentStep];
+        const speechOutput = step;
+        this.response.cardRenderer(SKILL_NAME, step);
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+    'PreviousStep': function () {
+        const factArr = macAndCheese;
+        currentStep = currentStep - 1;
+        const step = factArr[currentStep];
+        const speechOutput = step;
+        this.response.cardRenderer(SKILL_NAME, step);
         this.response.speak(speechOutput);
         this.emit(':responseReady');
     },
